45cb9acca0fb86deb37a819cf9942f8feedadf2c
José Costa <jose.costa@fraunhofer.pt>
2025-12-12 15:39:05 +0000

    Update actions

--- .github/scripts/semver_regex.py
+++ .github/scripts/semver_regex.py
@@ -1,37 +1,27 @@
-import os, re, sys
+import os
+import re
+import sys
+
 
 def main():
     tag = os.getenv("TAG")
     if tag is None:
-        print('No tag provided')
+        print("No tag provided")
         sys.exit(1)
 
     semver = re.compile(
-        r"""
-        ^(0|[1-9]\d*)\.
-        (0|[1-9]\d*)\.
-        (0|[1-9]\d*)
-        (?:-
-            (
-                (?:0|[1-9]\d*|[0-9A-Za-z-]*[A-Za-z-][0-9A-Za-z-]*)
-                (?:\.
-                    (?:0|[1-9]\d*|[0-9A-Za-z-]*[A-Za-z-][0-9A-Za-z-]*)
-                )*
-            )
-        )?
-        (?:\+
-            ([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*)
-        )?
-        $
-        """,
-        re.VERBOSE,
+        r"(?P<major>0|[1-9]\d*)\."
+        r"(?P<minor>0|[1-9]\d*)\."
+        r"(?P<patch>0|[1-9]\d*)"
+        r"(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?"
+        r"(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
     )
 
-    gh_output_path = os.getenv('GITHUB_OUTPUT')
+    gh_output_path = os.getenv("GITHUB_OUTPUT")
     if gh_output_path is None:
         print("GITHUB_OUTPUT env var is empty")
         sys.exit(1)
-    
+
     print(f"GITHUB_OUTPUT: {gh_output_path}")
     if not semver.match(tag):
         print(f"Invalid SemVer: {tag}")
